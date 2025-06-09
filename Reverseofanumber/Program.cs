9007d903621f3059e96be3f7a3bc951c29ce925c
kumar6646 <sai.penusoft@gmail.com>
2025-06-09 16:34:04 +0530

    Add project files.

---
+++ Reverseofanumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverseofanumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int num=int.Parse(Console.ReadLine());
+            int rem, sum = 0;
+            while(num!=0)
+            {
+                rem = num % 10;
+                sum = (sum * 10) + rem;
+                num = num / 10;
+
+            }
+            Console.WriteLine("Reverse of a number is "+sum);
+        }
+    }
+}
